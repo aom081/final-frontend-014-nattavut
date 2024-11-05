c54a431f36a4142803a73050be0431fd05f18f56
aaom081 <matemsom@gmail.com>
2024-11-05 16:08:39 +0700

    "Updated package-lock.json, package.json, and vite.config.js files with new dependencies and configurations."

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/podfolio014",
-})
+  base: "/final-frontend-014-nattavut",
+});
